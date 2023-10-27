0263a39c678c1fb961403129f04ed1236d74a074
aravind.ln <aravind.ln@gavstech.com>
2023-10-27 21:28:58 +0530

    commit

---
+++ src/app/UserData.ts
@@ -0,0 +1,11 @@
+export class UserData
+{
+    firstName:string="Aravind";
+    lastName:string="LN";
+    location:string="Chennai";
+    company:string="Gavs";
+    salary:number = 3000;
+    dateofjoining:Date=new Date();
+    
+
+}
